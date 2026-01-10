c60f6463cb829604e7924873b9cede14ba2ca831
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2026-01-10 21:46:29 +0530

    added analytics of vercel

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { ContextProvider } from "@/components/ContextAPI";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -156,6 +156,7 @@ export default function RootLayout({ children }) {
       >
         <ContextProvider>
           {children}
+          <Analytics />
           <Toaster richColors />
         </ContextProvider>
       </body>
