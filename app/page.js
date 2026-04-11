1a653e4373180ef2b173f47a553df3e0a946627f
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2026-04-11 17:52:31 +0530

    added banner

--- app/page.js
+++ app/page.js
@@ -9,10 +9,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="bg-sky-800">
+      {/* <div className="bg-sky-800">
         <h2 className="text-center text-white font-bold">Meet our AI Assistant Kas. Try Now</h2>
-      </div>
+      </div> */}
+  
       <Navbar/>
+         <div className="bg-gradient-to-r from-emerald-600 mt-14 via-sky-700 to-slate-950 border-y border-indigo-500/20 py-1.5 px-4 flex items-center justify-center">
+  <p className="text-[13px] tracking-tight text-white">
+    <span className="bg-gradient-to-r from-white to-cyan-400 bg-clip-text text-transparent font-bold mr-2">
+      New Logic Chart:
+    </span> 
+    Visualize code flow instantly—stop reading, start seeing.
+  </p>
+</div>
       <GridBackgroundDemo />
 
 
